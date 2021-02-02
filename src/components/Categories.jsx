672e9b735d93d1b0b0c9de90b92c8d4c5d0c3529
Martin Roldan <martinroldan889@gmail.com>
2021-02-02 20:26:01 -0300

    Añadiendo imágenes con Webpack

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -5,7 +5,7 @@ import '../assets/styles/components/Categories.scss';
 
 const Categories = (props) => (
     <div className="Categories">
-        <h3 class="categories__title">Mi lista</h3>
+        <h3 class="categories__title">{props.title}</h3>
         {props.children}
     </div>
 );
