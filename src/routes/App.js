9b8900592b70319083ebec906f83ff016fe65409
Martin Roldan <martinroldan889@gmail.com>
2021-02-04 13:29:52 -0300

    Container Register con estilos y ruta agregada

--- src/routes/App.js
+++ src/routes/App.js
@@ -3,13 +3,17 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Home from '../containers/Home';
 import Login from '../containers/Login';
+import Register from '../containers/Register';
 
 
 const App = () => (
     <BrowserRouter>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/login" component={Login} />
+        <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/register" component={Register} />
+        </Switch>
     </BrowserRouter>   
-)
+);
 
 export default App;
