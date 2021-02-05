ffa3ab52d0526b7ff8dc67f15c37899a51b26466
Martin Roldan <martinroldan889@gmail.com>
2021-02-05 09:39:00 -0300

    Creando un Layout contenedor

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 
-import Header from '../components/Header';
 import Search from '../components/Search';
 import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
-import Footer from '../components/Footer';
 import useInitialState from '../hooks/useInitialState';
 
 import '../assets/styles/App.scss';
@@ -17,8 +15,7 @@ const Home = () => {
     const initialState = useInitialState(API);
 
     return (
-        <div className='App'>
-            <Header />
+        <>
             <Search />
             
             <Categories title="Mi Lista">
@@ -47,9 +44,7 @@ const Home = () => {
                     }
                 </Carousel>
             </Categories>
-
-            <Footer />
-        </div>
+        </>
     );
 }
 
