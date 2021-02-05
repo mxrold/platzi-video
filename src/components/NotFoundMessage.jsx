994d14841e96593889b847f547d129fa7d8a187d
Martin Roldan <martinroldan889@gmail.com>
2021-02-05 09:51:04 -0300

    Manejando enlaces con Link

--- src/components/NotFoundMessage.jsx
+++ src/components/NotFoundMessage.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import '../assets/styles/components/NotFoundMessage.scss';
 
@@ -8,7 +9,7 @@ const NotFoundMessage = () => (
     <section class="error">
         <h2 class="animated pulse">404</h2>
         <p>Page Not Found</p>
-        <a href="#">Back Home</a>
+        <Link to="#">Back Home</Link>
     </section>
 );
 
