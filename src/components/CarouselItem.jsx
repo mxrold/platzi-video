388aa98a392d657e50c294780c3dd7b6d56fe5be
Martin Roldan <martinroldan889@gmail.com>
2021-02-09 16:17:20 -0300

    Debuggeando nuestro proyecto (agregando validaciones a nuestro componente card)

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -12,7 +12,7 @@ import removeIcon from '../assets/static/remove-icon.png';
 
 
 const CarouselItem = (props) => {
-  const { id, cover, title, year, contentRating, duration } = props;
+  const { id, cover, title, year, contentRating, duration, isList } = props;
   
   const handleSetFavorite = () => {
     props.setFavorite({
@@ -34,18 +34,22 @@ const CarouselItem = (props) => {
               src={playIcon} 
               alt="Play Icon" 
             /> 
-            <img 
-              className="carousel-item__details--img" 
-              src={plusIcon} 
-              alt="Plus Icon" 
-              onClick={handleSetFavorite}
-            /> 
+
+            {isList ?
             <img 
               className="carousel-item__details--img" 
               src={removeIcon} 
               alt="Remove Icon" 
               onClick={handleDeleteFavorite}
-            /> 
+            />  :
+            <img 
+                className="carousel-item__details--img" 
+                src={plusIcon} 
+                alt="Plus Icon" 
+                onClick={handleSetFavorite}
+              /> 
+            } 
+            
           </div>
           <p className="carousel-item__details--title">{title}</p>
           <p className="carousel-item__details--subtitle">
