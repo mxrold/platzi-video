f5f2ca5affbf505e1e18eb710f5a65c868031d0f
Martin Roldan <martinroldan889@gmail.com>
2021-02-12 15:48:00 -0300

    Agregando metodos para el formulario de login. Recoger valores de los inputs y enviar formulario

--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import '../assets/styles/components/Login.scss';
@@ -7,32 +7,64 @@ import googleIcon from '../assets/static/google-icon.png';
 import twitterIcon from '../assets/static/twitter-icon.png';
 
 
-const Login = () => (
-    <section class="login">
-        <section className="login__container">
-        <h2>Inicia sesión</h2>
-        <form className="login__container--form">
-            <input className="input" type="text" placeholder="Correo" />
-            <input className="input" type="password" placeholder="Contraseña" />
-            <button className="button">Iniciar sesión</button>
-            <div className="login__container--remember-me">
-            <label>
-                <input type="checkbox" id="cbox1" value="first_checkbox" />Recuérdame
-            </label>
-            <Link to="/">Olvidé mi contraseña</Link>
-            </div>
-        </form>
-        <section className="login__container--social-media">
-            <div><img src={googleIcon} /> Inicia sesión con Google</div>
-            <div><img src={twitterIcon} /> Inicia sesión con Twitter</div>
+const Login = () => {
+    const [ form, setValues ] = useState({
+        email: '',
+    });
+
+    const handleInput = event => {
+        setValues({
+            ...form,
+            [event.target.name]: event.target.value
+        })
+    }
+
+    const handleSubmit = event => {
+        event.preventDefault();
+
+        console.log(form);
+    }
+
+    return (
+        <section class="login">
+            <section className="login__container">
+            <h2>Inicia sesión</h2>
+            <form className="login__container--form" onSubmit={handleSubmit}>
+                <input 
+                    className="input" 
+                    name="email"
+                    type="text" 
+                    placeholder="Correo"
+                    onChange={handleInput}
+                />
+                <input 
+                    className="input" 
+                    name="password"
+                    type="password" 
+                    placeholder="Contraseña"
+                    onChange={handleInput}
+                />
+                <button className="submit">Iniciar sesión</button>
+                <div className="login__container--remember-me">
+                <label>
+                    <input type="checkbox" id="cbox1" value="first_checkbox" />Recuérdame
+                </label>
+                <Link to="/">Olvidé mi contraseña</Link>
+                </div>
+            </form>
+            <section className="login__container--social-media">
+                <div><img src={googleIcon} /> Inicia sesión con Google</div>
+                <div><img src={twitterIcon} /> Inicia sesión con Twitter</div>
+            </section>
+            <p className="login__container--register">No tienes ninguna cuenta
+                <Link to="/register">
+                    Regístrate
+                </Link> 
+            </p>
+            </section>
         </section>
-        <p className="login__container--register">No tienes ninguna cuenta
-            <Link to="/register">
-                Regístrate
-            </Link> 
-        </p>
-        </section>
-  </section>
-);
+    );
+}
+    
 
 export default Login;
