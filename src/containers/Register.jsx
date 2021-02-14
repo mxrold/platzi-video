a5b750460ae4c82189857b77df78449b3af953ae
Martin Roldan <martinroldan889@gmail.com>
2021-02-14 12:46:21 -0300

    Capturando los eventos del formulario de register

--- src/containers/Register.jsx
+++ src/containers/Register.jsx
@@ -1,22 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import '../assets/styles/components/Register.scss';
 
 
-const Register = () => (
-    <section className='register'>
-        <section className='register__container'>
-            <h2>Regístrate</h2>
-            <form className='register__container--form'>
-                <input className='input' type='text' placeholder='Nombre' />
-                <input className='input' type='text' placeholder='Correo' />
-                <input className='input' type='password' placeholder='Contraseña' />
-                <button className='button'>Registrarme</button>
-            </form>
-            <Link to='/login'>Iniciar sesión</Link>
+const Register = () => {
+    const [ form, setValues ] = useState({
+       email: '',
+       name: '',
+       password: ''
+    });
+
+    const handleInput = event => {
+        setValues({
+            ...form,
+            [event.target.name]: event.target.value
+        })
+    }
+
+    const handleSubmit = event => {
+        event.preventDefault();
+
+        console.log(form)
+    }
+
+    return (
+        <section className='register'>
+            <section className='register__container'>
+                <h2>Regístrate</h2>
+                <form className='register__container--form' onSubmit={handleSubmit}>
+                    <input
+                        className='input' 
+                        name='name'
+                        type='text' 
+                        placeholder='Nombre'
+                        onChange={handleInput}
+                    />
+                    <input 
+                        className='input'
+                        name='email'
+                        type='text' 
+                        placeholder='Correo'
+                        onChange={handleInput}
+                    />
+                    <input 
+                        className='input'
+                        name='password'
+                        type='password' 
+                        placeholder='Contraseña' 
+                        onChange={handleInput}
+                    />
+                    <button className='button'>Registrarme</button>
+                </form>
+                <Link to='/login'>Iniciar sesión</Link>
+            </section>
         </section>
-	</section>
-);
+    );
+}
 
 export default Register;
