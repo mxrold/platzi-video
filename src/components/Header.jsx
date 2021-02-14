fdf45ba10783adea0ca88f8902d2b27ac244cbca
Martin Roldan <martinroldan889@gmail.com>
2021-02-14 11:44:56 -0300

    Usando la función de Gravatar creada para obtener el hash del email y mostrar la foto de perfil de usuario, ya sea si esta logueado o no

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,48 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import { connect } from 'react-redux';
+
+import gravatar from '../utils/gravatar';
+
 import '../assets/styles/components/Header.scss';
 import logo from '../assets/static/logo-platzi-video-BW2.png';
 import userIcon from '../assets/static/user-icon.png';
 
 
-const Header = () => (
-  <header className="header">
-    <Link to="/">
-      <img className="header__img" src={logo} alt="Platzi Video" />
-    </Link>
-    <div className="header__menu">
-      <div className="header__menu--profile">
-        <img src={userIcon} alt="" />
-        <p>Perfil</p>
-      </div>
-      <ul>
-        <li><Link to="/">Cuenta</Link></li>
-        <li><Link to="/">Cerrar Sesión</Link></li>
-      </ul>
-    </div>
-  </header>
-);
+const Header = props => {
+  const { user } = props;
 
-export default Header;
+  const hasUser = Object.keys(user).length > 0;
+
+  return (
+    <header className="header">
+      <Link to="/">
+        <img className="header__img" src={logo} alt="Platzi Video" />
+      </Link>
+      <div className="header__menu">
+        <div className="header__menu--profile">
+          {
+            hasUser ? 
+              <img src={gravatar(user.email)} alt={user.email} />
+            :
+              <img src={userIcon} alt="" />
+          }
+          <p>Perfil</p>
+        </div>
+        <ul>
+          <li><Link to="/">Cuenta</Link></li>
+          <li><Link to="/">Cerrar Sesión</Link></li>
+        </ul>
+      </div>
+    </header>
+  );
+}
+
+const mapStateToProps = state => {
+  return {
+    user: state.user
+  }
+}
+
+export default connect(mapStateToProps, null)(Header);
